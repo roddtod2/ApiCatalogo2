144119fd985a45803127a11c39fe7bd67eeb9594
Rodrigo Silva <rodrigo.silva014@hotmail.com>
2024-08-13 22:56:50 -0300

    Adicionar Controllers Adicionar Metodos HTTP

---
+++ ApiCatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,53 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext? _context;
+
+        public CategoriasController(AppDbContext? context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            var categoria = _context.Categorias.ToList();
+
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+            
+            return Ok(categoria);
+        }
+
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+
+
+            if (categoria == null)
+                return BadRequest();
+
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria",new {id=categoria.CategoriaId},categoria);
+        }
+
+        //[HttpPut]
+
+        //[HttpDelete]
+
+    }
+}
